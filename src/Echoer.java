34d0121994a6410adc3f7d72ef3a06ef07ab74c8
adil <mansari@buffalo.edu>
2012-10-15 18:41:05 -0400

    Iterative commit, random check

--- src/Echoer.java
+++ src/Echoer.java
@@ -303,7 +303,7 @@ class ConnectionList extends Thread implements Runnable {
 					//} catch (UnknownHostException e) {
 					//	e.printStackTrace();
 					//} catch (IOException e) {
-						//e.printStackTrace();
+						//e.printStackTrace();adad
 					//}
 				}
 				/*System.out.println("IP address= "+ tcpClient[p].getInetAddress().getHostAddress());
